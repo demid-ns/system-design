2a9a70b39a6db708caab94ed3f9c6ae6b13f8209
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2023-06-20 22:56:34 +0300

    Add mappings

---
+++ TimelineCache/TimelineCache/Profiles/TimelineProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TimelineCache.DTOs;
+using TimelineCache.Models;
+
+namespace TimelineCache.Profiles
+{
+    public class TimelineProfile : Profile
+    {
+        public TimelineProfile()
+        {
+            //source -> target
+            CreateMap<User, UserReadDto>();
+            CreateMap<Subscription, SubscriptionReadDto>();
+        }
+    }
+}
