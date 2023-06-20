221e8d4ca9caec10578827325cc85e2c30504866
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2023-06-20 22:56:53 +0300

    Add timeline controller

---
+++ TimelineCache/TimelineCache/Controllers/TimelineController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimelineCache.DTOs;
+using TimelineCache.Services;
+
+namespace TimelineCache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimelineController : ControllerBase
+    {
+        private readonly ITimelineService _timelineService;
+        private readonly IMapper _mapper;
+
+        public TimelineController(
+            ITimelineService timelineService,
+            IMapper mapper
+            )
+        {
+            _timelineService = timelineService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("user-most-followers")]
+        public ActionResult<UserReadDto> GetUserWithMostFollowers()
+        {
+            var user = _timelineService.GetUserWithMostFollowers();
+            var userDto = _mapper.Map<UserReadDto>(user);
+
+            return userDto;
+        }
+    }
+}
