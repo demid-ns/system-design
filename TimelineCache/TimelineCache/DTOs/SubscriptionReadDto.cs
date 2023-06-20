7402211e9d3e002768b08b27d96ff3231ccf57e8
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2023-06-20 22:55:33 +0300

    Add dtos

---
+++ TimelineCache/TimelineCache/DTOs/SubscriptionReadDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using TimelineCache.Models;
+
+namespace TimelineCache.DTOs
+{
+    public class SubscriptionReadDto
+    {
+        public int Id { get; set; }
+        public int FollowerId { get; set; }
+        public int FollowingId { get; set; }
+    }
+}
---
+++ TimelineCache/TimelineCache/DTOs/UserReadDto.cs
@@ -0,0 +1,12 @@
+﻿using TimelineCache.Models;
+
+namespace TimelineCache.DTOs
+{
+    public class UserReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<SubscriptionReadDto> Followers { get; set; }
+        public ICollection<SubscriptionReadDto> Following { get; set; }
+    }
+}
